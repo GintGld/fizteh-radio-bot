3a2a6c2b1713f821396270e75305891ae1d6c560
GintGld <gliaudialis.ga@phystech.edu>
2024-03-20 17:05:46 +0300

    delete oapi script

--- scripts/oapi-codegen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# radio API
-oapi-codegen --config docs/radio/types.yaml docs/radio/openapi.yaml
-oapi-codegen --config docs/radio/client.yaml docs/radio/openapi.yaml
-
-# yandex API
-oapi-codegen --config docs/yandex/types.yaml docs/yandex/openapi.yaml
-oapi-codegen --config docs/yandex/client.yaml docs/yandex/openapi.yaml
